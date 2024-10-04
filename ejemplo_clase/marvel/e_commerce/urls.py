62e9b9c0ab2601e738eeff592cc71a5ea02d3d21
FChamorroMendoza <138070520+FChamorroMendoza@users.noreply.github.com>
2024-10-03 19:08:39 -0500

    Delete ejemplo_clase directory

--- ejemplo_clase/marvel/e_commerce/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from e_commerce.views import *
-
-
-urlpatterns = [
-    path('comic-list/', comic_list_api_view),
-    path('comic-retrieve/', comic_retrieve_api_view),
-    path('comic-create/', comic_create_api_view)
-]
